5a180f29f7f3bdb6b2d547592dd7028ed808d045
John <no_mail>
2014-10-01 12:30:46 +0200

    started with note.hpp list of tasks to do

--- GE_Projekt_2/src/shared/Notes.hpp
+++ GE_Projekt_2/src/shared/Notes.hpp
@@ -2,72 +2,21 @@
 
 
 
+contenthandler
+	zip
+	minneshanteing
+	implementera in-ur laddning etc.
 
+grafik 
+	mesh
+	height map(s)
+	~lod-stöd
 
-// grafik
-	- shaders
-	- meshes
-	- textures
-	- materials
-	
-	- pass
-	- effekter
-	- fönsterhantering
-	- viewports
+program
+	lod-hantering
+	trådning, job & task via process-class
+	main loop
+	level-init och hantering
 
-
-// ljud
-	- HDR
-	- streaming
-	- filter
-
-// fysik
-	- skapa spline
-	- spara spline
-	- följ spline
-	- scengraf
-	- replays
-
-
-// levelhantering
-	- skapa level
-	- spara level
-	- ladda in level
-	- skriptning
-		- generera html-dokumentation, med tabbar etc. "proper" hemsida
-		- generera bindings i prebuild eller via ett eget program. 
-		
-
-// contenthantering
-	- referensräkning
-	- async m. callbacks
-
-
-// voxelhantering
-	- generering
-		- multipla nivåer av perlin för voxlar, marknivå och biomer
-		- vatten
-		- texturer
-		- ambience
-
-	- spara
-	- streama
-
-	- lod-nivåer
-
-
-// utility
-	- camera
-	- tidtagning
-	- dynamisk länkning av dll
-	- profilering
-	- fönsterhantering
-	- input
-	- trådpool och system för trådning, m. prioritering etc.
-
-	- minneshantering
-
-	- fixed frames, replay-loggning
-	- event-pump
 
 */
