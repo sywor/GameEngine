3fa792cbfd310932a6b47e67f20b8ee59b2809ab
John <johy10@H473-0010.students.ad.bth.se>
2014-10-02 12:40:52 +0200

    Added threadpool

---
+++ GE_Projekt_2/src/core/Core/ThreadPool/Worker.cpp
@@ -0,0 +1,36 @@
+#include "Worker.hpp"
+#include "Threadpool.hpp"
+
+#include <functional>
+
+#include <iostream>
+
+
+void trr::Worker::operator()()
+{
+	std::function< void() > task;
+
+	while( true )
+	{
+		{
+			std::unique_lock<std::mutex> lock( m_pool.m_queueMutex );
+			m_pool.m_nrJobsInProgress--;
+
+			m_pool.m_waitCondition.notify_all();
+
+			while( !m_pool.m_stop && m_pool.m_tasks.empty() )
+			{
+				m_pool.m_condition.wait( lock );
+			}
+
+			if( m_pool.m_stop )
+				return;
+
+			m_pool.m_nrJobsInProgress++;
+			task = m_pool.m_tasks.front();
+			m_pool.m_tasks.pop_front();
+		}
+
+		task();
+	}
+}
