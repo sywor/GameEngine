1546b6541fff9d604daa27a24f6949c37ca2e69b
kraiper <martin_svart@hotmail.com>
2014-10-09 13:15:39 +0200

    Try this

--- BaseWindow/BaseWindow/VertexShader.hlsl
+++ BaseWindow/BaseWindow/VertexShader.hlsl
@@ -2,20 +2,36 @@
 
 struct VS_INPUT
 {
+	float4 pos		: POSITION;
+	float4 normal	: NORMAL;
+	float2 tex		: TEXCOORD0;
+};
+
+struct PS_INPUT
+{
 	float4 pos		: SV_POSITION;
 	float4 normal	: NORMAL;
 	float2 tex		: TEXCOORD0;
 };
 
 
-
-VS_INPUT main(VS_INPUT _input)
+PS_INPUT main(VS_INPUT _input)
 {
-	VS_INPUT output = (VS_INPUT)0;
+	PS_INPUT output = (PS_INPUT)0;
 
-	output.pos = mul(_input.pos,world);
-	output.pos = mul(output.pos, view);
-	output.pos = mul(output.pos, projection);
+
+	matrix ViewProjection = mul(view, projection);
+	matrix worldviewproj = mul(world, ViewProjection);
+
+	output.pos = mul(_input.pos, worldviewproj);
+
+
+	//output.pos = mul(_input.pos, world);
+	//output.pos = mul(output.pos, view);
+	//output.pos = mul(output.pos, projection);
+
+
+
 
 	output.normal = mul(_input.normal, world);
 	output.normal = mul(output.normal, view);
