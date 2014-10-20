76f9bd6864d5ab1794660ce99691f4dd505b7338
sywor <ossian.p@gmail.com>
2014-10-20 14:45:06 +0200

    I MADE THIS!!!

---
+++ PackageMaker/PackageMaker/Folder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageMaker
+{
+    class Folder
+    {
+        private List<Folder> subFolders = new List<Folder>();
+        private List<Asset> assets = new List<Asset>();
+        private string name;
+
+        public Folder(string _name)
+        {
+            name = _name;
+        }        
+    }
+}
