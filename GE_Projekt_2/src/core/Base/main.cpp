93134228160b3c47e0995718f54d7e489bc0f626
sywor <ossian.p@gmail.com>
2014-11-03 14:22:17 +0100

    Final test of full file load from package

--- GE_Projekt_2/src/core/Base/main.cpp
+++ GE_Projekt_2/src/core/Base/main.cpp
@@ -46,8 +46,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 	__int64 prevTimeStamp = 0;
 	QueryPerformanceCounter((LARGE_INTEGER*)&prevTimeStamp);
 
-	trr::contentManager.InitContentLib(L"tImage.zip");
-	trr::contentManager.GetResource("smiley.bmp.image");
+	trr::contentManager.InitContentLibs({"zlib128-dll.Spud","zlib128-dll.zip"});
+	trr::contentManager.GetResource("test/minigzip_d.exe.image");
 	// Main message loop
 	MSG msg = { 0 };
 	while (WM_QUIT != msg.message)
--- GE_Projekt_2/src/core/Core/ContentHandler/ContainerLoaders/PackageResult.hpp
+++ GE_Projekt_2/src/core/Core/ContentHandler/ContainerLoaders/PackageResult.hpp
@@ -1,4 +1,7 @@
 #pragma once
+#include <string>
+
+#define PR_TOSTRING(x) #x
 
 enum class PackageResult
 {
@@ -10,3 +13,30 @@ enum class PackageResult
 	ZIP_INVALID_CENTRALDIR_HEADER,
 	ZIP_ROOTDIR_NOT_FOUND
 };
+
+static std::string PackageResultToString(PackageResult _pr)
+{
+	switch (_pr)
+	{
+	case PackageResult::PACKAGE_OK:
+		return PR_TOSTRING(PACKAGE_OK);
+
+	case PackageResult::OUT_OF_MEMORY:
+		return PR_TOSTRING(OUT_OF_MEMORY);
+
+	case PackageResult::FILE_NOT_FOUND:
+		return PR_TOSTRING(FILE_NOT_FOUND);
+
+	case PackageResult::DIRECTORY_NOT_FOUND:
+		return PR_TOSTRING(DIRECTORY_NOT_FOUND);
+
+	case PackageResult::ZIP_INVALID_END_HEADER:
+		return PR_TOSTRING(ZIP_INVALID_END_HEADER);
+
+	case PackageResult::ZIP_INVALID_CENTRALDIR_HEADER:
+		return PR_TOSTRING(ZIP_INVALID_CENTRALDIR_HEADER);
+
+	case PackageResult::ZIP_ROOTDIR_NOT_FOUND:
+		return PR_TOSTRING(ZIP_ROOTDIR_NOT_FOUND);
+	}
+}
